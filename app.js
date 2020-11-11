d334bc1a5d78067a07e762d946ee17facb7fa0a5
joey <joefuller000@gmail.com>
2020-11-10 19:18:59 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv/config');
+
+app.use(bodyParser.json());
+
+//Import Routes
+const postsRoute = require('./routes/posts');
+app.use('/posts', postsRoute);
+
+//ROUTES
+app.get('/', (req, res) =>{
+    res.send('test');
+});
+
+app.get('/posts', (req, res) =>{
+    res.send('posts');
+});
+
+//Connect To DB
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    { useNewUrlParser: true }, 
+    () => console.log('connected to DB!')
+); 
+
+//Listening
+app.listen(3000);
